22c46251889cfd05358ebc39a2e34fbc0d397d3b
nickgravish <nick.gravish@gmail.com>
2015-04-28 14:42:34 -0400

    First working version of MPI code

--- PIV_MPI_cluster.py
+++ PIV_MPI_cluster.py
@@ -11,34 +11,35 @@ import sys
 import ctypes
 import sys
 import scipy.io as sio
-from mpi4py import MPI
+from PIL import Image
+import pypar as pp
 
-nproc = MPI.COMM_WORLD.Get_size() # Size of communicator
-iproc = MPI.COMM_WORLD.Get_rank() # Ranks in communicator
-inode = MPI.Get_processor_name() # Node where this MPI process runs
+num_processors = pp.size()
+rank = pp.rank()
+node = pp.get_processor_name()
+
+# MPI Constants
+MASTER_PROCESS = 0
+WORK_TAG = 1
+DIE_TAG = 2
+
+DeltaT = 1 # frames to skip
+window_size = 24
+overlap = 12
 
 
-# looks for a set of tiff ordered tiff files, starts up a pool of MPI workers to process them 
 
-
-def PIVCompute(framepair, window_size = 24, overlap = 12):
-    
-    a, b = framepair
-    frame_a = (shared_frames[a,:,:]).astype('int32')
-    frame_b = (shared_frames[b,:,:]).astype('int32')
+def PIVCompute(frame_a, frame_b, window_size = 24, overlap = 12):
     
     tmpu, tmpv, sig2noise = openpiv.pyprocess.piv(frame_a, frame_b,
                     window_size=window_size, overlap=overlap, dt=1, 
                     sig2noise_method='peak2peak', corr_method = 'direct')
     
-    x, y = openpiv.process.get_coordinates( image_size=frame_a.shape, window_size=WINDSIZE, overlap=OVERLAP )
+    x, y = openpiv.process.get_coordinates( image_size=frame_a.shape, window_size=window_size, overlap=overlap)
     tmpu, tmpv, mask = openpiv.validation.sig2noise_val( tmpu, tmpv, sig2noise, threshold = 1.3)
     u, v = openpiv.filters.replace_outliers( tmpu, tmpv, method='localmean', max_iter=10, kernel_size=4)
-        
-    print "Waiting for " + str(framepair)
-    sys.stdout.flush()
-
-    return u, v, sig2noise
+    
+    return u, v
 
 
 ############################################################################################
@@ -46,28 +47,120 @@ def PIVCompute(framepair, window_size = 24, overlap = 12):
 
 if __name__ == '__main__':
     
-    comm = MPI.COMM_WORLD 
-    rank = comm.Get_rank()
-    size = comm.Get_size()
-    
-    path = os.getcwd()
-    
-    name = sys.argv[1]
-    
-        
-# Master process
-    if rank == 0:
+    vidpath = sys.argv[1]
+    foldername = os.getcwd().split('/')[-1]
 
-        print "rank == 0 : " + name 
-        
-        # parse input folder to be processed
+    tif_files = [f for f in os.listdir(vidpath) if f.endswith('.tif')]
 
-# slave processes
+# !!!! DEBUG
+    tif_files = tif_files[:10]
+
+
+# Create list of file pairs to process
+    process_list = zip(range(0,len(tif_files)), range(DeltaT,len(tif_files)-DeltaT))
+    work_size = len(process_list)
+    
+    # Dispatch jobs to worker processes
+    work_index = 0
+    num_completed = 0
+
+    # Master process
+    if rank == MASTER_PROCESS:
+
+        frame_a = np.array(Image.open(os.path.join(vidpath, tif_files[0])));
+        frame_b = np.array(Image.open(os.path.join(vidpath, tif_files[1])));
+
+        # run PIV computation to get size of matrix and x,y's
+        x, y = openpiv.process.get_coordinates(image_size=frame_a.shape,
+                                                 window_size=window_size, overlap=overlap)
+
+        # pre-allocate the u, v matrices
+        u = np.zeros((work_size, x.shape[0], x.shape[1]))
+        v = np.zeros((work_size, x.shape[0], x.shape[1]))
+
+        # Start all worker processes
+        for i in range(1, min(num_processors, work_size)):
+            pp.send(work_index, i, tag=WORK_TAG)
+            pp.send(process_list[work_index], i)
+            print "Sent process list " + str(process_list[i]) + " to processor " + str(i)
+            work_index += 1
+
+        # Receive results from each worker, and send it new data
+        for i in range(num_processors, work_size):
+            results, status = pp.receive(source=pp.any_source, tag=pp.any_tag, return_status=True)
+            index = status.tag
+
+            # receive and parse the resulting var
+            u[index,:,:] = results[0,:,:]
+            v[index,:,:] = results[1,:,:]
+
+            proc = status.source
+            num_completed += 1
+            work_index += 1
+            pp.send(work_index, proc, tag=WORK_TAG)
+            pp.send(process_list[work_index], proc)
+            print "Sent work index " + str(work_index) + " to processor " + str(proc)
+
+        # Get results from remaining worker processes
+        while num_completed < work_size-1:
+            results, status = pp.receive(source=pp.any_source, tag=pp.any_tag, return_status=True)
+            index = status.tag
+            
+            # receive and parse the resulting var
+            u[index,:,:] = results[0,:,:]
+            v[index,:,:] = results[1,:,:]
+
+            num_completed += 1
+
+        # Shut down worker processes
+        for proc in range(1, num_processors):
+            print "Stopping worker process " + str(proc)
+            pp.send(-1, proc, tag=DIE_TAG)
+
+        # Package up the results to save, also save all the PIV parameters
+        sio.savemat(os.path.join(os.getcwd(), '../' + 'test.mat'),{'x':x, 'y':y, 'u':u, 'v':v, 
+                                                                    'window_size':window_size,
+                                                                    'overlap':overlap})
+
     else:
-        print 'rank = %d ' % MPI.COMM_WORLD.Get_rank()
+        ### Worker Processes ###
+        continue_working = True
+        while continue_working:
 
+            # check if being put to sleep
+            work_index, status =  pp.receive(source=MASTER_PROCESS, tag=pp.any_tag, 
+                    return_status=True)
 
-MPI.Finalize()
+            if status.tag == DIE_TAG:
+                continue_working = False
+            
+            # not being put to sleep, load in videos of interest and compute
+            else:
+                frame_pair, status = pp.receive(source=MASTER_PROCESS, tag=pp.any_tag, 
+                    return_status=True)
+                work_index = status.tag
+                
+                print  "Received work frame pair " + str(frame_pair)                 
+
+                frame_a = np.array(Image.open(os.path.join(vidpath, tif_files[frame_pair[0]])));
+                frame_b = np.array(Image.open(os.path.join(vidpath, tif_files[frame_pair[1]])));
+                
+                # Code below simulates a task running
+                u, v = PIVCompute(frame_a, frame_b, window_size = window_size, overlap = overlap)
+                
+                # package up into work array
+                work_array = np.zeros((2,u.shape[0], u.shape[1]))
+                work_array[0,:,:] = u
+                work_array[1,:,:] = v
+
+                result_array = work_array.copy()
+
+                pp.send(result_array, destination=MASTER_PROCESS, tag=work_index)
+        #### while
+    #### if worker
+
+    pp.finalize()
+
 
 
 
